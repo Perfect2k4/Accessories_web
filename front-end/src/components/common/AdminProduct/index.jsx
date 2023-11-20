8940f36fba3229c2325c4563eb9d3f35a69cc148
Hao <kenaru3s@gmail.com>
2023-11-20 13:37:27 +0700

    code new product

--- front-end/src/components/common/AdminProduct/index.jsx
+++ front-end/src/components/common/AdminProduct/index.jsx
@@ -1,19 +1,17 @@
 import React, { useState } from "react";
 import { PlusOutlined } from "@ant-design/icons";
 import TableComponent from "../TableComponent";
-import { Modal } from "antd";
-import { Form } from "react-router-dom";
+import { Button, Input, Modal, Form } from "antd";
 
 const AdminProduct = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const handleOk = () => {
-    setIsModalOpen(false);
-  };
   const handleCancel = () => {
     setIsModalOpen(false);
   };
+
+  const onFinish = () => {};
   return (
-    <div>
+    <>
       <div className="heading-01 text-center">Product Management</div>
       <button
         onClick={() => setIsModalOpen(true)}
@@ -24,54 +22,66 @@ const AdminProduct = () => {
       <div className="mt-[16px]">
         <TableComponent />
       </div>
-      <Modal
-        title="Create Products"
-        open={isModalOpen}
-        onOk={handleOk}
-        onCancel={handleCancel}
-      >
-       <Form
-    name="basic"
-    labelCol={{ span: 8 }}
-    wrapperCol={{ span: 16 }}
-    style={{ maxWidth: 600 }}
-    initialValues={{ remember: true }}
-    onFinish={onFinish}
-    onFinishFailed={onFinishFailed}
-    autoComplete="off"
-  >
-    <Form.Item<FieldType>
-      label="Username"
-      name="username"
-      rules={[{ required: true, message: 'Please input your username!' }]}
-    >
-      <Input />
-    </Form.Item>
+      <Modal title="Create Products" open={isModalOpen} onCancel={handleCancel}>
+        <Form
+          name="basic"
+          labelCol={{ span: 8 }}
+          wrapperCol={{ span: 16 }}
+          style={{ maxWidth: 600 }}
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          autoComplete="off"
+        >
+          <Form.Item
+            label="Name"
+            name="name"
+            rules={[{ required: true, message: "Please input your name!" }]}
+          >
+            <Input />
+          </Form.Item>
 
-    <Form.Item<FieldType>
-      label="Password"
-      name="password"
-      rules={[{ required: true, message: 'Please input your password!' }]}
-    >
-      <Input.Password />
-    </Form.Item>
+          <Form.Item label="Image" name="image">
+            <Input />
+          </Form.Item>
 
-    <Form.Item<FieldType>
-      name="remember"
-      valuePropName="checked"
-      wrapperCol={{ offset: 8, span: 16 }}
-    >
-      <Checkbox>Remember me</Checkbox>
-    </Form.Item>
+          <Form.Item
+            label="Price"
+            name="price"
+            rules={[{ required: true, message: "Please input your Price!" }]}
+          >
+            <Input />
+          </Form.Item>
 
-    <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-      <Button type="primary" htmlType="submit">
-        Submit
-      </Button>
-    </Form.Item>
-  </Form>
+          <Form.Item
+            label="Rating"
+            name="rating"
+            rules={[{ required: true, message: "Please input your Rating!" }]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Description"
+            name="description"
+            rules={[
+              { required: true, message: "Please input your Description!" },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="text-black bg-blue-700"
+            >
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
       </Modal>
-    </div>
+    </>
   );
 };
 
